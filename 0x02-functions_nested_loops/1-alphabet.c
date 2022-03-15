6aaf8d015d223b0eb32d6038caf3a46e4ff1a33e
Wanjiru12 <cmwai774@gmail.com>
2022-03-15 14:59:59 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Function main entry
+*
+* Program that prints _putchar, followed by a new line
+*
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
