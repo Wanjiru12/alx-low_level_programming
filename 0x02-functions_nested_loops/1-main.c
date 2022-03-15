5cd2c1ca617621ecb2e8095427c55253a6226871
Wanjiru12 <cmwai774@gmail.com>
2022-03-15 15:14:14 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - Function main entry
+*
+* Program that print_alphabets, followed by a new line
+*
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
